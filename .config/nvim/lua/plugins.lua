87459aaf2ce3375feb40cb2192d7a0be300f269f
James Wang <veriathon@gmail.com>
2025-06-08 09:46:02 -0700

    Removing lsp block in plugins, since it's moved

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -191,13 +191,3 @@ require('telescope').setup({
   }
 })
 
--- LSP configuration
-vim.lsp.config("lua_ls", {
-  settings = {
-    Lua = {
-      diagnostics = {
-	globals = { "vim" },
-      },
-    }
-  }
-})
