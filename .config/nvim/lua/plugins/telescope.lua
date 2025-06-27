7db8cc56ebd1004243623ec4cb4ff37ca904e253
James Wang <veriathon@gmail.com>
2025-06-27 14:10:15 -0700

    Adding smartcase for telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,17 +2,44 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" }
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require('telescope').setup{
+        defaults = {
+          -- use ripgrep’s smart-case
+          vimgrep_arguments = {
+            "rg",
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+            "--hidden",
+          },
+        },
+        extensions = {
+          fzf = {
+            fuzzy = true,
+            override_generic_sorter = true,
+            override_file_sorter    = true,
+            case_mode = "smart_case",
+          },
+        },
+      }
+      -- load extensions after setup
+      require('telescope').load_extension('fzf')
+      require('telescope').load_extension('project')
+    end,
   },
+
   {
-    "nvim-telescope/telescope-fzf-native.nvim", build = "make",
-    config = function() require('telescope').load_extension('fzf') end,
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = "make",
   },
+
   {
     "nvim-telescope/telescope-project.nvim",
     dependencies = { "nvim-telescope/telescope.nvim" },
-    config = function()
-      require("telescope").load_extension("project")
-    end,
-  }
+  },
 }
