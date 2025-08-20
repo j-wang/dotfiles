afecd1e53a5b3d0920892368e367f3c83d35ddad
James Wang <veriathon@gmail.com>
2025-08-20 12:30:43 -0700

    Bug: ansible commands now run on mac-dev-playbook

--- macossetup/init.sh
+++ macossetup/init.sh
@@ -4,5 +4,5 @@ git clone git@github.com:j-wang/mac-dev-playbook.git
 
 pip install ansible && \
 	xcode-select --install && \
-	ansible-galaxy install -r requirements.yml && \
-	ansible-playbook main.yml -i inventory -K
+	ansible-galaxy install -r ./mac-dev-playbook/requirements.yml && \
+	ansible-playbook ./mac-dev-playbook/main.yml -i inventory -K
